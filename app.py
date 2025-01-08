d9de7b82e577a0e5d89c5a48060782b81752b71f
M Hema Varshni <hemavarshnimanivannan2389@gmail.com>
2025-01-08 22:46:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,22 @@ import streamlit as st
 from transformers import BartTokenizer, BartForConditionalGeneration
 import torch
 
+# Replace this with your model link or path
+model_path = "https://drive.google.com/drive/folders/12ERv3Tl22Ukici91HH0Lp5qw8y-yL_bI?usp=drive_link"
+tokenizer = BartTokenizer.from_pretrained(model_path)
+model = BartForConditionalGeneration.from_pretrained(model_path)
+
 # Title
 st.title("Headline Generator")
 
 # Sidebar for Model Selection
 use_gpu = torch.cuda.is_available()
-
+'''
 # Load Model and Tokenizer
 model_name = "facebook/bart-base"
 tokenizer = BartTokenizer.from_pretrained("headline_generator_model")
 model = BartForConditionalGeneration.from_pretrained("headline_generator_model")
-
+'''
 device = torch.device("cuda" if use_gpu else "cpu")
 model.to(device)
 
